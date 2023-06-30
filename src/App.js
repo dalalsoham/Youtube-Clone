69cee465c06028c96eceee9d16b919ff47c8c85b
Dipanwita Biswas <biswasdipanwita.2001@gmail.com>
2023-06-30 22:33:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>lets build youtube clone</h1>
+      {/* header */}
+      <Header />
+      {/* Sidebar */}
+      {/* recommended videos */}
+
+
+      {/* these 3 are components */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+  return (
+    <div className='header' >
+      <h1>I am header</h1>
+      <MenuIcon />
+      <img className='header_logo' src="https://www.nicepng.com/png/detail/891-8915842_youtube-logo-free-png-small-youtube-logo-png.png" alt="" />
+      <input type="text" />
+      {/* <SearchIcon /> */}
+    </div>
+  )
+}
+
+export default Header
